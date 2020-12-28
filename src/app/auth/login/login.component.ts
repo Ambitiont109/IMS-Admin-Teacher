51e0108860892617d188e990567b5083f0e86bf9
ambition <ambition.t109@gmail.com>
2020-12-27 23:39:26 -0300

    add show/hide password button to password input

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,12 @@ import { Router } from '@angular/router';
 import { AuthService } from "../../auth.service";
 import { UsersService } from '../../@core/services/users.service';
 import { NbMenuService } from '@nebular/theme';
+
+enum PwdIconName{
+  show="eye",
+  hide="eye-off"
+}
+
 @Component({
   selector: 'ngx-login',
   templateUrl: './login.component.html',
@@ -19,10 +25,16 @@ export class LoginComponent implements OnInit {
   user: any = {};
   submitted: boolean = false;
   rememberMe = false;
+  is_password_shown:boolean=false;
+  password_icon_name=PwdIconName.show;
+  pwdType:string="password";
 
   constructor(protected service: AuthService,protected router: Router, private userService:UsersService) { }
 
   ngOnInit(): void {
+    this.is_password_shown = false;
+    this.password_icon_name = PwdIconName.show;
+    this.pwdType = "password";
   }
 
   login(): void {
@@ -49,5 +61,16 @@ export class LoginComponent implements OnInit {
       this.showMessages['error']=true
     }
     );
+  }
+  onEyeClicked(){
+    this.is_password_shown = !this.is_password_shown;
+    if(this.is_password_shown) {
+      this.password_icon_name = PwdIconName.hide;
+      this.pwdType = "text";
+    }
+    else {
+      this.password_icon_name = PwdIconName.show;    
+      this.pwdType = "password";
+    }
   }
 }
